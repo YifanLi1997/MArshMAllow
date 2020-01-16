8d9266775cc850e5644c65dbe90eb0e8ad99968e
李一帆 <liyifan@liyifandeMacBook-Pro.local>
2020-01-16 10:41:17 +0800

    Separated Enemy Pathing from Enemy

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,42 +6,21 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [SerializeField] int health = 500;
-    [SerializeField] float moveSpeed = 1f;
-    [SerializeField] List<GameObject> wavepoints;
 
-    int targetIndex = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        transform.position = wavepoints[targetIndex].transform.position;
-        targetIndex++;
+        
     }
+
+    
 
     // Update is called once per frame
     void Update()
     {
-        Move();
+
     }
 
-    void Move()
-    {
-        if(targetIndex < wavepoints.Count)
-        {
-            var currentPos = transform.position;
-            var targetPos = wavepoints[targetIndex].transform.position;
-            transform.position = Vector2.MoveTowards(
-                currentPos,
-                targetPos,
-                moveSpeed * Time.deltaTime);
-            if(transform.position == targetPos)
-            {
-                targetIndex++;
-            }
-        }
-        else
-        {
-            Destroy(gameObject);
-        }
-    }
+
 }
---
+++ Assets/Scripts/EnemyPathing.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPathing : MonoBehaviour
+{
+    [SerializeField] WaveConfig waveConfig;
+
+    List<Transform> wavepoints = new List<Transform>();
+    int targetIndex = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetWavePoints();
+
+        transform.position = wavepoints[targetIndex].position;
+        targetIndex++;
+    }
+
+    private void GetWavePoints()
+    {
+        var path = waveConfig.getPathPrefab();
+        foreach (Transform wavepoint in path.transform)
+        {
+            wavepoints.Add(wavepoint); // must initiate before Add
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        if (targetIndex < wavepoints.Count)
+        {
+            var currentPos = transform.position;
+            var targetPos = wavepoints[targetIndex].position;
+            var moveSpeed = waveConfig.getMoveSpeed();
+
+            transform.position = Vector2.MoveTowards(
+                currentPos,
+                targetPos,
+                moveSpeed * Time.deltaTime);
+            if (transform.position == targetPos)
+            {
+                targetIndex++;
+            }
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
